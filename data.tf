9fdc47e5211c05a5a368f964bd51938322bd696b
Chris Stotler <cstotler@abinitio.com>
2025-03-21 16:16:28 -0400

    Correct locals definition

--- data.tf
+++ data.tf
@@ -1,8 +1,8 @@
 locals {
-  api_gw_enabled = local.is_govcloud || var.api_gw_enabled = true
+  api_gw_enabled = local.is_govcloud || var.api_gw_enabled == true ? toset(["enabled"]) : []
   is_govcloud     = data.aws_partition.current.partition == "aws-us-gov"
   each_govcloud   = local.is_govcloud ? toset(["enabled"]) : []
-  each_commercial = local.is_govcloud || var.api_gw_enabled = true ? [] : toset(["enabled"])
+  each_commercial = local.is_govcloud || var.api_gw_enabled == true ? [] : toset(["enabled"])
 }
 
 data "aws_partition" "current" {}
